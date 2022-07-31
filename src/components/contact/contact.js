265c4a53605b45f3373cf803a836368bbfea342c
JOY <joy.kemunto@student.moringaschool.com>
2022-07-31 16:22:43 +0300

    changes

---
+++ src/components/contact/contact.js
@@ -0,0 +1,12 @@
+const Contact = props => {
+    return(
+        <div className="contact-info">
+            <div><i class="fas fa-envelope"></i>contact@email.com</div>
+            <div><i class="fas fa-envelope"></i>contact@email.com</div>
+            <div><i class="fas fa-envelope"></i>contact@email.com</div>
+            <div><i class="fas fa-envelope"></i>contact@email.com</div>
+            </div>
+    )
+}
+
+export default Contact
